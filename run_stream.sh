e8a8c331821a4841f6376740dc82787de07a1dbb
ilmars <9987548+darksworm@users.noreply.github.com>
2020-12-20 21:14:32 +0200

    add user friendly live redirect, check video list before starting stream

--- run_stream.sh
+++ run_stream.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 set -e
 
-uplink=rtmp:127.0.0.1/show/live?pwd=change_this_password
-videos=(prestream.mp4 keyframed_movie.mp4)
+uplink=rtmp:127.0.0.1/show/stream?pwd=change_this_password
+videos=(prestream.mp4) #keyframed_movie.mp4)
+
+for item in ${videos[*]}
+do
+    [ ! -f "export/$item" ] && echo "File export/$item does not exist!" && exit 1
+done
 
 for item in ${videos[*]}
 do
